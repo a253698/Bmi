ef9707e181ab3633b882445fd9ececf8f21e8974
Mavis <angel253698@gmail.com>
2018-10-09 16:05:30 +0800

    Bmi Alert

--- app/src/main/java/mavis/com/bmi/MainActivity.java
+++ app/src/main/java/mavis/com/bmi/MainActivity.java
@@ -1,5 +1,7 @@
 package mavis.com.bmi;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,8 +19,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void bmi(View view){
         Log.d("MainActivity" , "bmi");
-        EditText edWeight = findViewById(R.id.ed_weight);
-        EditText edHeight = findViewById(R.id.ed_height);
+        final EditText edWeight = findViewById(R.id.ed_weight);
+        final EditText edHeight = findViewById(R.id.ed_height);
         //取得輸入方塊元件
         String w = edWeight.getText().toString();
         String h = edHeight.getText().toString();
@@ -31,7 +33,15 @@ public class MainActivity extends AppCompatActivity {
         Log.d("MainActivity", bmi + "");
 
         Toast.makeText(this, "Your BMI is " + bmi, Toast.LENGTH_LONG).show();
-        // 最後一定要show出來 charSequence 就是 String 的父類別
+        // 浮字出來  最後一定要show出來 charSequence 就是 String 的父類別
+        new AlertDialog.Builder(this).setMessage("Your BMI is" + bmi).setTitle("BMI").setPositiveButton("OK!",null).setNegativeButton("OK!", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                edWeight.setText("");
+                edHeight.setText("");
+            }
+        }).show();
+        //跳視窗   support為支援，可以向下相容   builder為 alertDialog 專用
 
     }
 }
